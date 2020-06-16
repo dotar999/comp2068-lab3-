7402f061786d002d8235d409454d4ddda84b3c70
Vladyslav Serdiuk <200406142@student.georgianc.on.ca>
2020-06-15 23:52:01 -0400

    comp2068-lab3-

---
+++ lab3-/lab3-/routes/index.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/Vlad', (req, res) => {
+    res.render('Vlad', { title: "Vladyslav Serdiuk" });
+});
+
+router.get('/Natalia', (req, res) => {
+    res.render('Natalia', { title: "Natalia Serdiuk" });
+});
+
+router.get('/Igor', (req, res) => {
+    res.render('Igor', { title: "Igor Serdiuk" });
+});
+
+module.exports = router;
